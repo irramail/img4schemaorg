454ac76e45f912cc170d370ae09a63f51913c607
Alex Krivolutsky <irramail@gmail.com>
2020-07-21 20:33:47 +0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use std::process::Command;
+extern crate redis;
+
+use redis::{Commands};
+use jsonrpc_http_server::jsonrpc_core::{IoHandler, Value, Params, Error};
+use jsonrpc_http_server::{ServerBuilder};
+
+fn parse_arguments (p: Params) -> Result<Vec<String>, Error> {
+  let mut result = Vec::new();
+  match p {
+    Params::Array(array) => {
+      for s in &array {
+        match s {
+          Value::String(s) => result.push(s.clone()),
+          _ => return Err(Error::invalid_params("expecting strings"))
+        }
+      }
+    }
+    _ => return Err(Error::invalid_params("expecting an array of strings"))
+  }
+  if result.len() < 1 {
+    return Err(Error::invalid_params("missing api key"));
+  }
+
+  return Ok(result[0..].to_vec());
+}
+
+fn fetch_img(img: &str) -> redis::RedisResult<isize> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+  let img = format!("{}", img);
+
+  let _ : () = con.set("schemaJpg", img)?;
+
+  let mut echo_hello = Command::new("sh");
+  echo_hello.arg("-c").arg("/home/p6/scripts/schemaJpg.sh").spawn().expect("sh command failed to start");
+
+  con.get("schemaJpg")
+}
+
+fn exists_img() -> redis::RedisResult<bool> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.exists("schemaImg")
+}
+
+fn set_first_run() -> redis::RedisResult<isize> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  let _ : () = con.del("schemaJpg")?;
+
+  con.get("mayBeSomeKey")
+}
+
+fn main() {
+  let mut io = IoHandler::new();
+
+  let _ = set_first_run();
+
+  io.add_method("set_img",  move |params: Params| {
+    let w = parse_arguments(params)?;
+    let _ = fetch_img( &w[0]);
+
+    Ok(Value::String("".to_string()))
+  });
+
+  io.add_method("exists_img",  | _params | {
+    let bool = exists_img().unwrap().to_string();
+    Ok(Value::String(bool))
+  });
+
+  let server = ServerBuilder::new(io)
+  .threads(3)
+  .start_http(&"127.0.0.1:3032".parse().unwrap())
+  .unwrap();
+
+  server.wait();
+}
