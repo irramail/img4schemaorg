b9ab144668d53419fcb33c0de746842067b4247a
Alex Krivolutsky <irramail@gmail.com>
2020-07-29 03:44:43 +0700

    update schemaImg.sh with ammonite.io/scala

--- scripts/schemaImg.sh
+++ scripts/schemaImg.sh
@@ -1,42 +1,59 @@
-#!/bin/sh
-cd ~/schema
-rm -f *.jpg
-rm -f schema.tgz
-rm -f schemaImg.b64
-COUNTER=0
-FILENAME=`redis-cli get schemaImgFileName`
-redis-cli get schemaImg | sed "s/|/\n/g" | sed "s/^.*,//" >  ~/schema/schemaImg.b64
-for  i in `cat  ~/schema/schemaImg.b64`;
-do
-  COUNTER=$((COUNTER + 1))
-  echo $i | base64 -d > "$FILENAME"_"$COUNTER".jpg;
-  
-  /home/p6/scripts/aspectcrop -a 16:9 -g c "$FILENAME"_"$COUNTER".jpg "$FILENAME"_16_9_"$COUNTER".jpg
-  /home/p6/scripts/aspectcrop -a 4:3 -g c "$FILENAME"_"$COUNTER".jpg "$FILENAME"_4_3_"$COUNTER".jpg
-  /home/p6/scripts/aspectcrop -a 1:1 -g c "$FILENAME"_"$COUNTER".jpg "$FILENAME"_1_1_"$COUNTER".jpg
+#!/usr/bin/env amm
 
-  convert "$FILENAME"_"$COUNTER".jpg -resize '640' "$FILENAME"_o_640_"$COUNTER".jpg
-  convert "$FILENAME"_"$COUNTER".jpg -resize '1280' "$FILENAME"_o_1280_"$COUNTER".jpg
-  convert "$FILENAME"_"$COUNTER".jpg -resize '1920' "$FILENAME"_o_1920_"$COUNTER".jpg
+import $ivy.`net.debasishg:redisclient_2.13:3.30`
 
-  convert "$FILENAME"_16_9_"$COUNTER".jpg -resize 'x360' "$FILENAME"_16_9_640_"$COUNTER".jpg
-  convert "$FILENAME"_16_9_"$COUNTER".jpg -resize 'x480' "$FILENAME"_16_9_854_"$COUNTER".jpg
-  convert "$FILENAME"_16_9_"$COUNTER".jpg -resize 'x720' "$FILENAME"_16_9_1280_"$COUNTER".jpg
-  convert "$FILENAME"_16_9_"$COUNTER".jpg -resize 'x1080' "$FILENAME"_16_9_1920_"$COUNTER".jpg
+import com.redis._
+import java.io._
+import sys.process._
 
-  convert "$FILENAME"_4_3_"$COUNTER".jpg -resize 'x480' "$FILENAME"_4_3_640_"$COUNTER".jpg
-  convert "$FILENAME"_4_3_"$COUNTER".jpg -resize 'x960' "$FILENAME"_4_3_1280_"$COUNTER".jpg
-  convert "$FILENAME"_4_3_"$COUNTER".jpg -resize 'x1440' "$FILENAME"_4_3_1920_"$COUNTER".jpg
+val r = new RedisClient("127.0.0.1", 6379)
+val hd = System.getProperty("user.dir")
+val wd = hd + "/schema/"
+val listfiles = (s"ls $wd").!!
+listfiles.split('\n').foreach(fn => if (fn.length() > 1) (s"rm -f $wd$fn").!)
 
-  convert "$FILENAME"_1_1_"$COUNTER".jpg -resize '640' "$FILENAME"_1_1_640_"$COUNTER".jpg
-  convert "$FILENAME"_1_1_"$COUNTER".jpg -resize '1280' "$FILENAME"_1_1_1280_"$COUNTER".jpg
-  convert "$FILENAME"_1_1_"$COUNTER".jpg -resize '1920' "$FILENAME"_1_1_1920_"$COUNTER".jpg
+val fn = r.get("schemaImgFileName").getOrElse("schemaImgO")
+val wdfn = wd + fn
+var out = None: Option[FileOutputStream]
+out = Some(new FileOutputStream(wd + fn + "_1.jpg"))
 
-  tar -czf schema.tgz *.jpg
-  ln -s /home/p6/schema/"$FILENAME"_"$COUNTER".jpg schemaImg_"$COUNTER".jpg
-  redis-cli set schemaImg"$COUNTER"Width `identify -format "%[fx:w]" "$FILENAME"_"$COUNTER".jpg`
-  redis-cli set schemaImg"$COUNTER"Height `identify -format "%[fx:h]" "$FILENAME"_"$COUNTER".jpg`
-done
+out.get.write(java.util.Base64.getDecoder.decode(r.get("schemaImg").getOrElse(",").split(',')(1)))
 
-sleep 5
-redis-cli del schemaImg
+if (out.isDefined) out.get.close
+
+val u="_"
+val c = "1"
+for ((a, b, x, s)  <- List(("16", "9", "x", List(("640", "360"), ("854", "480"), ("1280", "720"), ("1920", "1080")))
+	, ("4", "3", "x", List(("640", "480"), ("1280", "960"), ("1920", "1440")))
+	, ("1", "1", " ", List(("640", "640"), ("1280", "1280"), ("1920", "1920")))
+	, ("o","o", " ", List(("640", "640"), ("1280", "1280"), ("1920", "1920"))))) {
+		if (a != "o") {
+			(s"$hd/scripts/aspectcrop -a $a:$b -g c $wdfn$u$c.jpg $wdfn$u$a$u$b$u$c.jpg").!
+		}
+		for( (ins) <- s) {
+			val sx = ins._1
+			val sy = ins._2
+			if (a != "o") {
+				(s"convert $wdfn$u$a$u$b$u$c.jpg -resize '$x$sy' $wdfn$u$a$u$b$u$sx$u$c.jpg").!
+			} else {
+				(s"convert $wdfn$u$c.jpg -resize '$x$sy' $wdfn$u$a$u$sx$u$c.jpg").!
+			}
+		}
+}
+val lnfn = "schemaImg"
+(s"ln -s $wdfn$u$c.jpg $wd$lnfn$u$c.jpg").!
+
+val imgWidth=(s"identify -format '%[fx:w]' $wdfn$u$c.jpg").!!
+val imgHeight=(s"identify -format '%[fx:h]' $wdfn$u$c.jpg").!!
+
+r.set(s"schemaImg$c"+"Width", imgWidth.stripLineEnd)
+r.set(s"schemaImg$c"+"Height", imgHeight.stripLineEnd)
+
+val fntgz = "schema.tgz"
+val tmpfn = s"/tmp/$fntgz"
+(s"tar -czf $tmpfn -C $wd .").!
+(s"mv $tmpfn $wd").!
+
+("sleep 5").!
+
+r.del("schemaImg")
