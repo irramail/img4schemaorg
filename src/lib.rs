bdef762dc2a5cde0338df103522047ad23aea90c
Alex Krivolutsky <irramail@gmail.com>
2020-08-03 20:29:38 +0700

    move div_creator + dependencies to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use url::{Url, ParseError};
+extern crate redis;
+
+use redis::{Commands};
+
 pub fn div(aw:i32, ah:i32, w:i32, h:i32, url: String, fname: String, description: String) -> String {
   format!("<div itemscope=\"\" itemtype=\"http://schema.org/ImageObject\" itemprop=\"thumbnail\" style=\"display:none;\">
     <link itemprop=\"contentUrl\" href=\"{url}/{fname}_{aw}_{ah}_{w}_1.jpg\">
@@ -16,6 +21,122 @@ pub fn gen_srcset(path :&str, fname: &str) -> String {
           format!("{}_o_1280_1.jpg 1280w", fullpath),
           format!("{}_o_1920_1.jpg 1920w", fullpath)
   )
+}
+
+fn get_path(url : &str) -> Result<Url, ParseError> {
+  let parsed = Url::parse(url)?;
+  Ok(parsed)
+}
+
+fn get_url() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImgURL")
+}
+
+fn get_fname() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImgFileName")
+}
+
+fn get_alt() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImgAlt")
+}
+
+fn get_description() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImgDescription")
+}
+
+fn get_meta_name() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImgMetaName")
+}
+
+fn get_meta_description() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImgMetaDescription")
+}
+
+fn get_width() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImg1Width")
+}
+
+fn get_height() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImg1Height")
+}
+
+fn get_aspect_resolution() -> redis::RedisResult<String> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
+  let mut con = client.get_connection()?;
+
+  con.get("schemaImgAspectResolution")
+}
+
+pub fn div_creator() -> String {
+  let url= get_url().unwrap();
+  let path = get_path(url.as_str()).unwrap();
+  let fname= get_fname().unwrap();
+  let alt = get_alt().unwrap();
+  let description =  get_description().unwrap();
+
+  let srcset = gen_srcset(path.path(), fname.as_str());
+
+  let img = format!("<img decoding=\"async\" itemprop=\"contentUrl\" sizes=\"(max-width: 1280px) 320px, 640px, 1280px\" {}\nsrc=\"{}/{}_1.jpg\"\nalt=\"{}\">", srcset, path.path(), fname.as_str(), alt);
+
+  let meta_name = format!("<meta itemprop=\"name\" content=\"{}\">", get_meta_name().unwrap());
+  let meta_description =  format!("<meta itemprop=\"description\" content=\"{}\">", get_meta_description().unwrap());
+
+  //println!("{} {}", get_width().unwrap(), get_height().unwrap());
+  let meta_width_height = format!("<meta itemprop=\"width\" content=\"{}px\">\n<meta itemprop=\"height\" content=\"{}px\">", get_width().unwrap(), get_height().unwrap());
+
+  let ares = get_aspect_resolution().unwrap();
+
+  let mut div_all : String = "".to_string();
+  //1:1_640x640,1280x1280,1920x1920;4:3_640x480,1280x960,1920x1440;16:9_640x360,854x480,1280x720,1920x1080
+  for ar in ares.split_terminator(';') {
+    //1:1_640x640,1280x1280,1920x1920
+    let item_ar : Vec<&str> = ar.split_terminator('_').collect();
+
+    //1:1
+    let a : Vec<&str> = item_ar[0].split_terminator(':').collect();
+
+    //1 1
+    let a_w = a[0];
+    let a_h = a[1];
+
+    //640x640,1280x1280,1920x1920
+    for res in item_ar[1].split_terminator(',') {
+      let item_res : Vec<&str> = res.split_terminator('x').collect();
+      let res_w = item_res[0];
+      let res_h = item_res[1];
+
+      div_all = format!("{}{}", div_all, div(a_w.parse().unwrap(), a_h.parse().unwrap(), res_w.parse().unwrap(), res_h.parse().unwrap(), url.clone(), fname.clone(), description.clone()));
+    }
+
+  }
+
+  let bwrapper = "<div itemprop=\"image\" itemscope=\"\" itemtype=\"http://schema.org/ImageObject\" class=\"ImageObject_cont\">";
+  let ewrapper = "</div>";
+  format!("{}\n{}\n{}\n{}\n{}\n{}\n{}", bwrapper, img, meta_name, meta_description, meta_width_height, div_all, ewrapper)
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::process::Command;
-use url::{Url, ParseError};
 extern crate redis;
 
 use redis::{Commands};
@@ -28,8 +27,6 @@ fn parse_arguments (p: Params) -> Result<Vec<String>, Error> {
   return Ok(result[0..].to_vec());
 }
 
-
-
 fn set_url_and_fname(url_and_fname: &str) -> redis::RedisResult<isize> {
   let client = redis::Client::open("redis://127.0.0.1/")?;
   let mut con = client.get_connection()?;
@@ -52,127 +49,9 @@ fn set_url_and_fname(url_and_fname: &str) -> redis::RedisResult<isize> {
   con.get("schemaImgFileName")
 }
 
-fn get_url() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImgURL")
-}
-
-fn get_fname() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImgFileName")
-}
-
-fn get_description() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImgDescription")
-}
-
-fn get_width() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImg1Width")
-}
-
-fn get_height() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImg1Height")
-}
-
-fn get_path(url : &str) -> Result<Url, ParseError> {
-  let parsed = Url::parse(url)?;
-  Ok(parsed)
-}
-
-fn get_meta_name() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImgMetaName")
-}
-
-fn get_meta_description() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImgMetaDescription")
-}
-
-fn get_alt() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImgAlt")
-}
-
-fn get_aspect_resolution() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://127.0.0.1/")?;
-  let mut con = client.get_connection()?;
-
-  con.get("schemaImgAspectResolution")
-}
-
 fn run_script() {
   let mut echo_hello = Command::new("sh");
   let _status = echo_hello.arg("-c").arg("/home/p6/scripts/schemaImg.sh").status().expect("sh command failed to start");
-}
-
-fn divcreator() -> String {
-
-
-  let url= get_url().unwrap();
-  let path = get_path(url.as_str()).unwrap();
-  let fname= get_fname().unwrap();
-  let alt = get_alt().unwrap();
-  let description =  get_description().unwrap();
-
-  let srcset = gen_srcset(path.path(), fname.as_str());
-
-  let img = format!("<img decoding=\"async\" itemprop=\"contentUrl\" sizes=\"(max-width: 1280px) 320px, 640px, 1280px\" {}\nsrc=\"{}/{}_1.jpg\"\nalt=\"{}\">", srcset, path.path(), fname.as_str(), alt);
-
-  let meta_name = format!("<meta itemprop=\"name\" content=\"{}\">", get_meta_name().unwrap());
-  let meta_description =  format!("<meta itemprop=\"description\" content=\"{}\">", get_meta_description().unwrap());
-
-  //println!("{} {}", get_width().unwrap(), get_height().unwrap());
-  let meta_width_height = format!("<meta itemprop=\"width\" content=\"{}px\">\n<meta itemprop=\"height\" content=\"{}px\">", get_width().unwrap(), get_height().unwrap());
-
-  let ares = get_aspect_resolution().unwrap();
-
-  let mut div_all : String = "".to_string();
-  //1:1_640x640,1280x1280,1920x1920;4:3_640x480,1280x960,1920x1440;16:9_640x360,854x480,1280x720,1920x1080
-  for ar in ares.split_terminator(';') {
-    //1:1_640x640,1280x1280,1920x1920
-    let item_ar : Vec<&str> = ar.split_terminator('_').collect();
-
-    //1:1
-    let a : Vec<&str> = item_ar[0].split_terminator(':').collect();
-
-    //1 1
-    let a_w = a[0];
-    let a_h = a[1];
-
-    //640x640,1280x1280,1920x1920
-    for res in item_ar[1].split_terminator(',') {
-      let item_res : Vec<&str> = res.split_terminator('x').collect();
-      let res_w = item_res[0];
-      let res_h = item_res[1];
-
-      div_all = format!("{}{}", div_all, div(a_w.parse().unwrap(), a_h.parse().unwrap(), res_w.parse().unwrap(), res_h.parse().unwrap(), url.clone(), fname.clone(), description.clone()));
-    }
-
-  }
-
-  let bwrapper = "<div itemprop=\"image\" itemscope=\"\" itemtype=\"http://schema.org/ImageObject\" class=\"ImageObject_cont\">";
-  let ewrapper = "</div>";
-  format!("{}\n{}\n{}\n{}\n{}\n{}\n{}", bwrapper, img, meta_name, meta_description, meta_width_height, div_all, ewrapper)
 }
 
 fn fetch_img(img: &str) -> redis::RedisResult<isize> {
@@ -184,7 +63,7 @@ fn fetch_img(img: &str) -> redis::RedisResult<isize> {
   let _ : () = con.set("backupSchemaImg", img)?;
 
   run_script();
-  let _ : () = con.set( "schemaOrg", divcreator())?;
+  let _ : () = con.set( "schemaOrg", div_creator())?;
 
   con.get("schemaImg")
 }
@@ -210,7 +89,7 @@ fn retry() -> redis::RedisResult<bool> {
   let _ : () = con.set( "schemaImg", backup_schema_img().unwrap())?;
 
   run_script();
-  let _ : () = con.set( "schemaOrg", divcreator())?;
+  let _ : () = con.set( "schemaOrg", div_creator())?;
 
   con.exists("backupSchemaImg")
 }
